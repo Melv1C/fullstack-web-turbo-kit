ea4f8cb9dcc7f48fa32ab19eaeba2415113ecdb4
Melvyn Claes <claes.melvyn@gmail.com>
2026-05-22 14:51:19 +0200

    chore: remove commitlint configuration and husky from package.json

--- commitlint.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ["@commitlint/config-conventional"],
-  ignores: [
-    (message) => message.startsWith("Merge "),
-    (message) => message.startsWith("Revert "),
-    (message) => message.startsWith("fixup! "),
-    (message) => message.startsWith("squash! "),
-  ],
-};
